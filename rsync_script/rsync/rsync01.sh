89fce726ea28a2ab7417e730566bc83112f118c0
Terence Lau <terenceliu1012@gmail.com>
2019-01-19 02:39:57 +0800

    rsync_script

---
+++ rsync_script/rsync/rsync01.sh
@@ -0,0 +1,9 @@
+echo "$(date + %Y-%m%-%d_%H:%M:%S) syncing" >> /rsync_script/status/01.txt
+
+$Push_Source = "cran.r-project.org"
+$Push_Target = "/mnt/mirrors/CRAN/"
+rsync -rtlzv --delete cran.r-project.org::CRAN /mnt/mirrors/CRAN/
+
+vi /var/log/$Push_Target.log
+echo "$(date +%Y-%m-%d_%H:%M:%S) -The Data Directoty:$Push_Source to localhost had Sync Sucess" >>/var/log/$Push_Target.log
+
