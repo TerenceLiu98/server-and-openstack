3e4daeff4b1c3a86712b894e467be059f7e15daa
Terence Lau <terenceliu1012@gmail.com>
2019-01-18 11:30:16 +0800

    reconstructed & rewrite script

--- rsync_script/rsync/rsync02.sh
+++ rsync_script/rsync/rsync02.sh
@@ -1,3 +1,5 @@
+echo "$(date + %Y-%m%-%d_%H:%M:%S) syncing" >> /rsync_script/status/02.txt
+
 $Push_Source = "rsync://mirrors.tuna.tsinghua.edu.cn/CTAN/"
 $Push_Target = "/mnt/mirrors/CTAN/"
 
@@ -6,3 +8,4 @@ rsync -rtlzv --delete rsync://mirrors.tuna.tsinghua.edu.cn/CTAN/
 
 vi /var/log/$Push_Target.log
 echo "$(date +%Y-%m-%d_%H:%M:%S) -The Data Directoty:$Push_Source to localhost had Sync Sucess" >>/var/log/$Push_Target.log
+
